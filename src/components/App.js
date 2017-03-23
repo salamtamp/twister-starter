e64f0b4bacda21035aa518a10e4b4dcbcb045172
Patchara Suwanwanakul <salamtam.p@gmail.com>
2017-03-23 09:32:55 +0700

    Upload code 1st day

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,8 @@
 import React from 'react'
+import MainPanel from './MainPanel'
 
 const App = () => (
-  <div className="container">
-    <div className="jumbotron">
-      <h1 style={{ color: '#000' }}>Hello, world!</h1>
-      <p>...</p>
-      <p>
-        <a className="btn btn-primary btn-lg" href="/">Learn more</a>
-      </p>
-    </div>
-  </div>
+  <MainPanel />
 )
 
 export default App
---
+++ src/components/NewTweet.js
@@ -0,0 +1,107 @@
+import React, { Component, PropTypes } from 'react'
+import config from '../config'
+
+class NewTweet extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      tweetText: '',
+    }
+
+    this.handleOnChange = this.handleOnChange.bind(this)
+    this.handleOnKeyDown = this.handleOnKeyDown.bind(this)
+    this.handleOnClick = this.handleOnClick.bind(this)
+    this.addTweet = this.addTweet.bind(this)
+  }
+
+  handleOnChange(event) {
+    this.setState({
+      tweetText: event.target.value,
+    })
+  }
+
+  handleOnKeyDown(event) {
+    if (event.keyCode !== 13) {
+      return
+    }
+    event.preventDefault()
+    this.addTweet({
+      name: this.props.name,
+      username: this.props.username,
+      tweetText: this.state.tweetText,
+    })
+  }
+
+  handleOnClick() {
+    this.addTweet({
+      name: this.props.name,
+      username: this.props.username,
+      tweetText: this.state.tweetText,
+    })
+  }
+
+  addTweet(tweet) {
+    fetch(`http://${config.api.host}:${config.api.port}/api/tweets`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      mode: 'cors',
+      body: JSON.stringify(tweet),
+    })
+    .then(response => response.json())
+    .then((data) => {
+      this.setState({
+        tweetText: '',
+      })
+      this.props.addToTweetList(data)
+    })
+  }
+
+  render() {
+    return (
+      <div className="new-tweet">
+        <form className="form-horizontal">
+          <div className="form-group">
+            <div className="tweet-text col-sm-10">
+              <input
+                type="text"
+                id="tweetText"
+                className="form-control"
+                placeholder="What's happening"
+                value={this.state.tweetText}
+                onChange={this.handleOnChange}
+                onKeyDown={this.handleOnKeyDown}
+              />
+            </div>
+            <div className="col-sm-2">
+              <input
+                type="button"
+                className="btn btn-default"
+                value="Tweet"
+                onClick={this.handleOnClick}
+              />
+            </div>
+          </div>
+        </form>
+      </div>
+    )
+  }
+}
+
+NewTweet.propTypes = {
+  name: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired,
+  addToTweetList: PropTypes.arrayOf(PropTypes.object).isRequired,
+}
+
+NewTweet.defaultProps = {
+  name: 'Patchara S.',
+  username: 'salamtam',
+  tweetText: 'Untitled',
+}
+
+
+export default NewTweet
